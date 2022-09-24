94fc11f193815f094208504ed3a2d3d66f23de1d
Matthieu <matthieunadeau96@gmail.com>
2022-09-24 11:50:30 -0400

    Populate HomeScreen with sample tasks

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:todos/models/todo_model.dart';
 import 'package:todos/screens/add_todo_screen.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -28,7 +27,62 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(child: Text('Home')),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: SizedBox(
+                child: Text(
+                  'Pending Todos',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: Todo.todos.length,
+              itemBuilder: ((context, index) {
+                return _todoCard(Todo.todos[index]);
+              }),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Card _todoCard(Todo todo) {
+    return Card(
+      margin: const EdgeInsets.only(bottom: 8.0),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              '#${todo.id}: ${todo.task}',
+            ),
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.check),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.close),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
