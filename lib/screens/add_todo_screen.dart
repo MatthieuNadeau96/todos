74145c5e9ce89060b1ac71d7825ee4e31d896f30
Matthieu <matthieunadeau96@gmail.com>
2022-09-24 11:51:06 -0400

    Build AddTodoScreen with TextFormFields

--- lib/screens/add_todo_screen.dart
+++ lib/screens/add_todo_screen.dart
@@ -1,18 +1,67 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:todos/models/todo_model.dart';
 
 class AddTodoScreen extends StatelessWidget {
   const AddTodoScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController controllerId = TextEditingController();
+    TextEditingController controllerTask = TextEditingController();
+    TextEditingController controllerDescription = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('Add Todo'),
+        title: const Text('Add a new task'),
       ),
-      body: const Center(child: Text('Add Todos')),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          children: [
+            _inputField('ID', controllerId),
+            _inputField('Task', controllerTask),
+            _inputField('Description', controllerDescription),
+            ElevatedButton(
+              onPressed: () {
+                var todo = Todo(
+                  id: controllerId.value.text,
+                  task: controllerId.value.text,
+                  description: controllerId.value.text,
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).primaryColor,
+              ),
+              child: const Text('Add Task'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _inputField(String field, TextEditingController controller) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '$field: ',
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Container(
+          height: 50,
+          margin: const EdgeInsets.only(
+            bottom: 10,
+          ),
+          child: TextFormField(
+            controller: controller,
+          ),
+        ),
+      ],
     );
   }
 }
